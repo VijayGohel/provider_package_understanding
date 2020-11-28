7a8f480ac7ce893bd23d6e2ecdcbfbdc81bbaf9f
vijay gohel <gohil5898@gmail.com>
2020-11-28 23:19:52 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('data'),
+        ),
+        body: Level1(),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Level2();
+  }
+}
+
+class Level2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Level3();
+  }
+}
+
+class Level3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('data');
+  }
+}
